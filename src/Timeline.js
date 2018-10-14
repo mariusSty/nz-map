81ba1871b071b1e66d4023f2d2908e748599ba2d
Marius STEPHANY <marius.stephany@gmail.com>
2018-10-14 20:21:28 +1300

    :sparkles: Change state when click on button of Timeline Component

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
 class Timeline extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Hello"
+    };
+  }
   render() {
     return (
       <li>
-        <button className="step" onClick={() => console.log("click")}>
-          {this.props.value}
+        <button
+          className="step"
+          onClick={() => this.setState({ value: "Click" })}
+        >
+          {this.state.value}
         </button>
       </li>
     );
