61b478fb0102e90697e9c0b5538075479a88dbc7
Marius STEPHANY <marius.stephany@gmail.com>
2018-10-31 00:50:37 +1300

    :sparkles: Update Button of Timeline to ListItem

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,6 +1,11 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import theme from "./theme.js";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 
 function Timeline(props) {
   let isActive;
@@ -9,27 +14,41 @@ function Timeline(props) {
     isActive = props.value[i] === props.current;
     classNameActive = isActive ? "active" : "";
     return (
-      <li key={i}>
-        <Button
-          variant="outlined"
-          color="secondary"
-          className={classNameActive}
-          onClick={() => props.onClick(i)}
+      <ListItem button key={i}>
+        <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="center"
         >
-          {steps.name}
-        </Button>
-      </li>
+          <Typography color="secondary">#{steps.order}</Typography>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography type="body2" color="secondary">
+                {steps.name}
+              </Typography>
+            }
+            onClick={() => props.onClick(i)}
+          />
+          <span />
+        </Grid>
+      </ListItem>
     );
   });
 
   return (
-    <div>
-      <Typography variant="h5" color="secondary">
-        Trip to New Zealand
-      </Typography>
-      <hr noshade="true" className="separator" />
-      <ul>{steps}</ul>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <Typography variant="h5" color="secondary">
+          Trip to New Zealand
+        </Typography>
+        <hr noshade="true" className="separator" />
+        <List color="primary" className="list">
+          {steps}
+        </List>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
