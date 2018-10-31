b6325ab54aa6b0f05eed46c29d480e8d0f882d19
Marius STEPHANY <marius.stephany@gmail.com>
2018-11-01 01:15:07 +1300

    :sparkles: Add datas for Omapere

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import theme from "./theme.js";
 import { MuiThemeProvider } from "@material-ui/core/styles";
+import MapIcon from "@material-ui/icons/Map";
 
 class App extends Component {
   constructor(props) {
@@ -25,7 +26,7 @@ class App extends Component {
         {
           order: 1,
           name: "Auckland",
-          dates: ["06/10/2018", "09/10/2018"],
+          dates: ["06/10/2018", "08/10/2018"],
           img: [
             "./img/Auckland/IMG_0543.JPG",
             "./img/Auckland/IMG_0544.JPG",
@@ -43,14 +44,26 @@ class App extends Component {
           imgWidth: [],
           thumbnailHeight: [],
           thumbnailWidth: [],
-          caption: [],
+          caption: [
+            "Silverfern Backpackers (1/2)",
+            "Silverfern Backpackers (2/2)",
+            "Mount Eden (1/5)",
+            "Mount Eden (2/5)",
+            "Mount Eden (3/5)",
+            "Mount Eden (4/5)",
+            "Mount Eden (5/5)",
+            "Bruce Lee Sushi (1/2)",
+            "Bruce Lee Sushi (2/2)",
+            "Auckland War Memorial Museum (1/2)",
+            "Auckland War Memorial Museum (2/2)"
+          ],
           longitude: -36.848461,
           latitude: 174.763336
         },
         {
           order: 2,
           name: "Whangarei",
-          dates: ["09/10/2018", "17/01/2018"],
+          dates: ["09/10/2018", "13/10/2018"],
           img: [
             "./img/Whangarei/IMG_0561.JPG",
             "./img/Whangarei/IMG_0562.JPG",
@@ -69,19 +82,34 @@ class App extends Component {
           imgWidth: [],
           thumbnailHeight: [],
           thumbnailWidth: [],
-          caption: [],
+          caption: [
+            "The Cell Block Backpackers (1/3)",
+            "The Cell Block Backpackers (2/3)",
+            "The Cell Block Backpackers (3/3)",
+            "Abbey Caves",
+            "Skatepark Whangarei (1/2)",
+            "Skatepark Whangarei (2/2)",
+            "Mair Park (1/6)",
+            "Mair Park (2/6)",
+            "Mair Park (3/6)",
+            "Mair Park (4/6)",
+            "Mair Park (5/6)",
+            "Mair Park (6/6)"
+          ],
           longitude: -35.725113,
           latitude: 174.323715
         },
         {
           order: 3,
           name: "Paihia",
-          dates: ["15/01/2018", "17/01/2018"],
+          dates: ["14/10/2018", "18/10/2018"],
           img: [
+            "./img/Paihia/IMG_0611.JPG",
             "./img/Paihia/IMG_0618.JPG",
             "./img/Paihia/IMG_0619.JPG",
             "./img/Paihia/IMG_0624.JPG",
             "./img/Paihia/IMG_0625.JPG",
+            "./img/Paihia/IMG_0626.JPG",
             "./img/Paihia/IMG_0652.JPG",
             "./img/Paihia/IMG_0654.JPG"
           ],
@@ -89,46 +117,76 @@ class App extends Component {
           imgWidth: [],
           thumbnailHeight: [],
           thumbnailWidth: [],
-          caption: [],
+          caption: [
+            "Hundertwasser Toilets",
+            "Bay Adventurer Backpackers",
+            "Paihia Beach (1/3)",
+            "Paihia Beach (2/3)",
+            "Paihia Beach (3/3)",
+            "St Pauls Anglican Church",
+            "Paihia City (1/2)",
+            "Paihia City (2/2)"
+          ],
           longitude: -35.2821,
           latitude: 174.091
         },
         {
           order: 4,
           name: "Kaeo",
-          dates: ["15/01/2018", "17/01/2018"],
+          dates: ["19/10/2018", "23/10/2018"],
           img: [
             "./img/Kaeo/IMG_0666.JPG",
             "./img/Kaeo/IMG_0667.JPG",
             "./img/Kaeo/IMG_0668.JPG",
-            "./img/Kaeo/IMG_0671.JPG",
             "./img/Kaeo/IMG_0672.JPG",
-            "./img/Kaeo/IMG_0673.JPG",
-            "./img/Kaeo/IMG_0674.JPG",
-            "./img/Kaeo/IMG_0679.JPG",
-            "./img/Kaeo/IMG_0680.JPG",
+            "./img/Kaeo/IMG_0675.JPG",
+            "./img/Kaeo/IMG_0676.JPG",
+            "./img/Kaeo/IMG_0677.JPG",
+            "./img/Kaeo/IMG_0678.JPG",
             "./img/Kaeo/IMG_0681.JPG",
             "./img/Kaeo/IMG_0682.JPG",
             "./img/Kaeo/IMG_0683.JPG",
+            "./img/Kaeo/IMG_0684.JPG",
+            "./img/Kaeo/IMG_0685.JPG",
+            "./img/Kaeo/IMG_0687.JPG",
+            "./img/Kaeo/IMG_0689.JPG",
+            "./img/Kaeo/IMG_0690.JPG",
+            "./img/Kaeo/IMG_0692.JPG",
             "./img/Kaeo/IMG_0695.JPG",
-            "./img/Kaeo/IMG_0700.JPG",
-            "./img/Kaeo/IMG_0701.JPG",
-            "./img/Kaeo/IMG_0706.JPG",
-            "./img/Kaeo/IMG_0718.JPG",
             "./img/Kaeo/IMG_0720.JPG"
           ],
           imgHeight: [],
           imgWidth: [],
           thumbnailHeight: [],
           thumbnailWidth: [],
-          caption: [],
+          caption: [
+            "Farm - HelpX (1/13)",
+            "Farm - HelpX (2/13)",
+            "Farm - HelpX (3/13)",
+            "Farm - HelpX (4/13)",
+            "Farm - HelpX (5/13)",
+            "Farm - HelpX (6/13)",
+            "Farm - HelpX (7/13)",
+            "Farm - HelpX (8/13)",
+            "Farm - HelpX (9/13)",
+            "Farm - HelpX (10/13)",
+            "Farm - HelpX (11/13)",
+            "Farm - HelpX (12/13)",
+            "Farm - HelpX (13/13)",
+            "Whangaroa Harbour (1/5)",
+            "Whangaroa Harbour (2/5)",
+            "Whangaroa Harbour (3/5)",
+            "Whangaroa Harbour (4/5)",
+            "Whangaroa Harbour (5/5)",
+            "Hitchhiking"
+          ],
           longitude: -35.1005,
           latitude: 173.781
         },
         {
           order: 5,
           name: "Ahipara",
-          dates: ["18/01/2018"],
+          dates: ["24/10/2018", "26/10/2018"],
           img: [
             "./img/Ahipara/IMG_0721.JPG",
             "./img/Ahipara/IMG_0723.JPG",
@@ -142,20 +200,28 @@ class App extends Component {
           imgWidth: [],
           thumbnailHeight: [],
           thumbnailWidth: [],
-          caption: [],
+          caption: [
+            "Ahipara Beach (1/6)",
+            "Ahipara Beach (2/6)",
+            "Ahipara Beach (3/6)",
+            "Ahipara Beach (4/6)",
+            "Ahipara Beach (5/6)",
+            "Ahipara Beach (6/6)",
+            "Wairoa Stream"
+          ],
           longitude: -35.1713,
           latitude: 173.153
         },
         {
           order: 6,
           name: "Cape Reinga",
-          dates: ["15/01/2018", "17/01/2018"],
+          dates: ["26/10/2018"],
           img: [
+            "./img/Cape Reinga/IMG_0736.JPG",
+            "./img/Cape Reinga/IMG_0737.JPG",
+            "./img/Cape Reinga/IMG_0738.JPG",
             "./img/Cape Reinga/IMG_0739.JPG",
-            "./img/Cape Reinga/IMG_0740.JPG",
             "./img/Cape Reinga/IMG_0742.JPG",
-            "./img/Cape Reinga/IMG_0744.JPG",
-            "./img/Cape Reinga/IMG_0745.JPG",
             "./img/Cape Reinga/IMG_0749.JPG",
             "./img/Cape Reinga/IMG_0751.JPG"
           ],
@@ -163,9 +229,60 @@ class App extends Component {
           imgWidth: [],
           thumbnailHeight: [],
           thumbnailWidth: [],
-          caption: [],
+          caption: [
+            "Lighthouse (1/3)",
+            "Lighthouse (2/3)",
+            "Lighthouse (3/3)",
+            "Cape Reinga (1/4)",
+            "Cape Reinga (2/4)",
+            "Cape Reinga (3/4)",
+            "Cape Reinga (4/4)"
+          ],
           longitude: -34.44176,
           latitude: 172.70085
+        },
+        {
+          order: 7,
+          name: "Omapere",
+          dates: ["30/10/2018", "01/11/2018"],
+          img: [
+            "./img/Omapere/IMG_0760.JPG",
+            "./img/Omapere/IMG_0763.JPG",
+            "./img/Omapere/IMG_0775.JPG",
+            "./img/Omapere/IMG_0783.JPG",
+            "./img/Omapere/IMG_0785.JPG",
+            "./img/Omapere/IMG_0786.JPG",
+            "./img/Omapere/IMG_0787.JPG",
+            "./img/Omapere/IMG_0791.JPG",
+            "./img/Omapere/IMG_0793.JPG",
+            "./img/Omapere/IMG_0796.JPG",
+            "./img/Omapere/IMG_0797.JPG",
+            "./img/Omapere/IMG_0798.JPG",
+            "./img/Omapere/IMG_0801.JPG",
+            "./img/Omapere/IMG_0802.JPG"
+          ],
+          imgHeight: [],
+          imgWidth: [],
+          thumbnailHeight: [],
+          thumbnailWidth: [],
+          caption: [
+            "Hokianga Harbour (1/13)",
+            "Hokianga Harbour (2/13)",
+            "Globetrekkers Lodge",
+            "Hokianga Harbour (3/13)",
+            "Hokianga Harbour (4/13)",
+            "Hokianga Harbour (5/13)",
+            "Hokianga Harbour (6/13)",
+            "Hokianga Harbour (7/13)",
+            "Hokianga Harbour (8/13)",
+            "Hokianga Harbour (9/13)",
+            "Hokianga Harbour (10/13)",
+            "Hokianga Harbour (11/13)",
+            "Hokianga Harbour (12/13)",
+            "Hokianga Harbour (13/13)"
+          ],
+          longitude: -35.5351,
+          latitude: 173.388
         }
       ],
       current: null,
@@ -237,7 +354,10 @@ class App extends Component {
                 >
                   <MenuIcon />
                 </IconButton>
-                <Typography variant="h6">Trip to New Zealand</Typography>
+                <Typography variant="h6">
+                  <MapIcon className="title" />
+                  Trip to New Zealand
+                </Typography>
               </Toolbar>
             </AppBar>
           </Hidden>
